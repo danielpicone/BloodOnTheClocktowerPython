ea44cf88455c6f7be989dca21bd02bb3eb94f1a2
Daniel Picone <danielp@frontier-economics.com.au>
2021-08-01 18:05:23 +1000

    Various additions to bot functionality

--- bot.py
+++ bot.py
@@ -17,25 +17,38 @@ REACTIONS = {"player_role": "\U00002705",
              "noms": "\U0001F4DC"}
 REVERSE_REACTION_MAPPING = {y: x for (x,y) in REACTIONS.items()}
 
+PLAYING_MESSAGE = "Blood on the Clocktower"
+
+NOTIFICATION_CHANNEL = "commands"
+
 intents = discord.Intents.default()
 intents.members = True
 
 # client = discord.Client()
 bot = commands.Bot(command_prefix="!", intents = intents)
 
+MESSAGE_IDS = []
 
 @bot.event
 async def on_ready():
     print(f'{bot.user.name} has connected to Discord!')
 
-    text_channel = get_text_channel(GUILD, "moveeradmin")
+    game = discord.Game(PLAYING_MESSAGE)
+    await bot.change_presence(activity = game)
+
+    text_channel = get_text_channel(GUILD, NOTIFICATION_CHANNEL)
     message = await text_channel.send(
         embed = discord.Embed(title = "Blood on the Clocktower commands",
-                              description = "Use the emojis to move players between voice channels",
-                              colour = 9021952),
-                              delete_after = 3*3600)
+                              description = "Click the emojis to move players between voice channels\n" + \
+                                             "\U00002705:  gives the user the `Player` role, and unclicking removes the `Player` role\n" + \
+                                             "\U0001F319:  moves all users marked with the `Player` or `Story Teller` role to individual cottages\n" + \
+                                             "\U00002600:  moves all users marked with the `Player` or `Story Teller` role to `Beneath the Clocktower`\n" + \
+                                             "\U0001F4DC:  warns all users that nominations are soon and will move all users in `Ravenswood Bluff` to `Beneath the Clocktower`",
+                              colour = 2123412))
     for name, code in REACTIONS.items():
         await message.add_reaction(code)
+
+    return message
 
 
 @bot.command(name = "99", help = "Responds with a random quote from Brooklyn 99")
@@ -68,22 +81,33 @@ async def on_error(event, *args, **kwargs):
             raise
 
 @bot.command(name = "night")
-async def night(ctx, voice_channel="Beneath the Clocktower", category="Burbs"):
+async def night(ctx, voice_channel="Beneath the Clocktower", category_from = "Ravenswood Bluff", category_to="Burbs"):
     voice_channel = get_voice_channel(GUILD, voice_channel)
-    cat = get_category(GUILD, category)
+    cat_from = get_category(GUILD, category_from)
+    cat_to = get_category(GUILD, category_to)
 
-    num_cottages = len(cat.voice_channels)
+    num_cottages = len(cat_to.voice_channels)
     player_num = 1
 
+    # Get all members in Ravenswood Bluff
+    members = []
+    for vc in cat_from.voice_channels:
+        members.extend(vc.members)
+
+    # guild = get_guild(GUILD)
+
     # Move all story tellers to first voice_channel
-    for member in voice_channel.members:
+    # for member in voice_channel.members:
+    for member in members:
         member_roles = [n.name for n in member.roles]
+        # print("Story Teller" in member_roles)
+        # print(discord.utils.get(guild.roles, name = "Story Teller") in member.roles)
         if "Story Teller" in member_roles:
-            await member.move_to(cat.voice_channels[0])
+            await member.move_to(cat_to.voice_channels[0])
         elif "Player" in member_roles:
-            await member.move_to(cat.voice_channels[player_num])
+            await member.move_to(cat_to.voice_channels[player_num])
             player_num += 1
-            if player_num > len(cat.voice_channels):
+            if player_num > len(cat_to.voice_channels):
                 await ctx.send("Not enough cottages for all players")
 
 @bot.command(name = "day")
@@ -96,7 +120,7 @@ async def day(ctx, category="Burbs", voice_channel = "Beneath the Clocktower"):
 
 @bot.command(name = "noms")
 async def noms(ctx, category="Ravenswood Bluff", voice_channel = "Beneath the Clocktower",
-               notification_channel = "moveeradmin", delay = 10):
+               notification_channel = NOTIFICATION_CHANNEL, delay = 10):
     voice_channel = get_voice_channel(GUILD, voice_channel)
     cat = get_category(GUILD, category)
 
@@ -139,6 +163,7 @@ async def player_role(ctx, user, type = "add"):
 
 @bot.event
 async def on_reaction_add(reaction, user):
+    text_channel = get_text_channel(GUILD, NOTIFICATION_CHANNEL)
     if user.bot:
         return
 
@@ -146,7 +171,11 @@ async def on_reaction_add(reaction, user):
         await player_role(None, user, "add")
         return
 
-    await eval(REVERSE_REACTION_MAPPING[reaction.emoji] + "(None)")
+    if "Story Teller" in [role.name for role in user.roles]:
+        await eval(REVERSE_REACTION_MAPPING[reaction.emoji] + "(None)")
+    else:
+        await text_channel.send(user.display_name + " is trying to move members without the Story Teller role, shame!")
+
     await reaction.message.remove_reaction(reaction.emoji, user)
 
 @bot.event
@@ -185,5 +214,10 @@ def get_role(guild, role_name):
             for role in g.roles:
                 if role.name == role_name:
                     return role
+
+def get_guild(guild = GUILD):
+    for guild in bot.guilds:
+        if guild.name == GUILD:
+            return guild
 
 bot.run(TOKEN)
