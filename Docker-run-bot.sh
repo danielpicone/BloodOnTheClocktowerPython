9d9ed4828999688b118aa66bb342d9a3558dc947
Daniel Picone <danielpicone2@gmail.com>
2022-07-23 22:48:36 +1000

    Add ghrc login

--- Docker-run-bot.sh
+++ Docker-run-bot.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+sudo docker login ghcr.io --username danielpicone -p $CR_PAT
 echo "Pulling latest image from ghcr.io/danielpicone/bloodontheclocktowerpython:latest"
 sudo docker pull ghcr.io/danielpicone/bloodontheclocktowerpython:latest
 echo "Creating and running container in the background"
