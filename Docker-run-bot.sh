15e6ef7a4cb1a489d548ff646bc843dac49e1480
Daniel Picone <danielpicone2@gmail.com>
2022-07-23 22:27:49 +1000

    Remove -rm flag from bash script

--- Docker-run-bot.sh
+++ Docker-run-bot.sh
@@ -2,4 +2,4 @@
 echo "Pulling latest image from ghcr.io/danielpicone/bloodontheclocktowerpython:latest"
 sudo docker pull ghcr.io/danielpicone/bloodontheclocktowerpython:latest
 echo "Creating and running container in the background"
-sudo docker run --env-file .env --rm -d --restart=unless-stopped ghcr.io/danielpicone/bloodontheclocktowerpython:latest
+sudo docker run --env-file .env -d --restart=unless-stopped ghcr.io/danielpicone/bloodontheclocktowerpython:latest
